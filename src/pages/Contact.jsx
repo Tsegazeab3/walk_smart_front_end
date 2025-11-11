ce9b94d1ed0431c284ee18ac1a01227e3b18be16
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2025-11-11 13:31:53 +0400

    feat: Integrate Formspree for contact and preorder forms

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
         <h3 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center text-[#212121] mb-12">Contact Us</h3>
         <div className="max-w-lg mx-auto bg-white p-8 rounded-lg shadow-lg">
           <p className="text-lg text-gray-600 mb-8 text-center">Have questions or want to partner with us? Fill out the form below and we'll get back to you as soon as possible.</p>
-          <form>
+          <form action="https://formspree.io/f/mkgknbdo" method="POST">
             <div className="mb-4">
               <label htmlFor="name" className="block text-gray-700 font-bold mb-2">Name</label>
               <input type="text" id="name" name="name" className="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500" />
--- src/pages/Preorder.jsx
+++ src/pages/Preorder.jsx
@@ -26,18 +26,18 @@ const Preorder = () => {
                   <p className="text-gray-600">Special Pre-order Price</p>
                 </div>
 
-                <form>
+                <form action="https://formspree.io/f/mkgknbdo" method="POST">
                   <div className="mb-4">
                     <label htmlFor="name" className="block text-gray-700 font-bold mb-2">Full Name</label>
-                    <input type="text" id="name" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-green-500" />
+                    <input type="text" id="name" name="name" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-green-500" />
                   </div>
                   <div className="mb-4">
                     <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email Address</label>
-                    <input type="email" id="email" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-green-500" />
+                    <input type="email" id="email" name="email" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-green-500" />
                   </div>
                   <div className="mb-6">
                     <label htmlFor="country" className="block text-gray-700 font-bold mb-2">Country</label>
-                    <select id="country" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-green-500">
+                    <select id="country" name="country" className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-green-500">
                       <option>United Arab Emirates</option>
                       <option>United States</option>
                       <option>Canada</option>
